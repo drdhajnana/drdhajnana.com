56dafe77a3bc30d30e945e12dd1540a10e9978dc
vinayprakash2204 <vinay.h.prakash@gmail.com>
2022-07-07 14:40:48 +0530

    First Commit Project Tic Tac Toe

---
+++ StudioProjects/untitled/Tic-Tac-Toe-Player-First/lib/config/routes/routes_models.dart
@@ -0,0 +1,11 @@
+import 'package:tictactoeplayerfirst/constants/constants.dart';
+
+class RouteModels {
+  final String landingViewTitle;
+
+  RouteModels({
+    this.landingViewTitle = PageTitleConstants.landingScreenTitle,
+  });
+}
+
+
